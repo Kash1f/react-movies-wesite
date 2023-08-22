e73a072f12ec03fe1663f3d44a2d892d0e5ede56
Kashif <ahmed.kashif1001@gmail.com>
2023-08-22 21:48:21 +0500

    added-firstmovie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
+import Movie from './components/Moviee/Movie'
 
 function App() {
   return (
     <>
     <div className="App">
       <Header/>
+      <div className='main'>
+      <Movie/>
+      </div>
       </div>
     </>
   );
---
+++ src/components/Moviee/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const Movie = () => {
+  return (
+    <div className='movie'>
+      
+      <img src="https://ia.media-imdb.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="" />
+      <p>Star Wars: The Last Jedi</p>
+      <p>Year: 2007</p>
+
+
+    </div>
+  )
+}
+
+export default Movie
